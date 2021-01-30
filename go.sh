7f4c42bef348f0accb10e3c826cefe4c0ee7591b
PharaohCola13 <spencer.riley@student.nmt.edu>
2021-01-30 12:03:52 -0700

    major updates to auto

--- go.sh
+++ go.sh
@@ -1,47 +1,18 @@
 #! bin/bash/
-while getopts ":major:minor" opt; do
+while getopts "ac" opt; do
 	case "${opt}" in
-    major)
+    a)
 				sudo sed -i 's/raspberrypi/ursa-major/g' /etc/hostname
 				sudo sed -i 's/raspberrypi/ursa-major/g' /etc/hosts
-
-				sudo cp ./major/bluetooth-auto-power@.service /etc/systemd/system/
-				sudo sed -i 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd -C"/g' /etc/systemd/system/dbus-org.bluez.service
-
-				sudo cp ./major/10-local.rules /etc/udev/rules.d/
-				sudo cp ./major/obexpush.service /etc/systemd/system/
-				# sudo apt-get update && sudo apt-get upgrade
-				# sudo apt-get install bluez bluez-tools libbluetooth-dev gnome-bluetooth
-				# bt-adapter --set Discoverable 1
-				# bt-agent pair 5C:F3:70:9C:A5:0D
-				# bt-obex -p 5C:F3:70:9C:A5:0D
-				#bluetooth-sendto --device 5C:F3:70:9C:A5:0D ./data/file.txt
-
-				sudo service bluetooth stop
-				sudo bluetooth --compat
-
-				systemctl --user start obex
-				sudo systemctl --global enable obex
-				sudo systemctl enable obexpush
-
+                sudo mv ./major_src/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf 
 				sudo apt-get install python3-pip python3-dev libatlas-base-dev
 				sudo pip3 install numpy pyserial
 
-        cd ./iniarduino/
-        chmod 0755 install.sh
-        sudo ./install.sh
-        iniarduino
-        cd ../
-
 				sudo reboot
         ;;
-		minor)
-				sudo sed -i 's/raspberrypi/ursa-minor/g' /etc/hostname
-				sudo sed -i 's/raspberrypi/ursa-minor/g' /etc/hosts
-
-				{
-					sleep 10s
-					sudo killall obexpushd
-				} & sudo obexpushd -B23 -o /bluetooth -n
-			esac
-		done
+	c) 
+		vpnstart
+		vpnstop
+		;;
+	esac
+done
--- major_src/test.py
+++ major_src/test.py
@@ -1,39 +1,52 @@
 from numpy import *
 import serial, time
 from datetime import datetime, timedelta
+import subprocess
 
 tau      = 10 # Temporal resolution (1 measurement/tau seconds)
 dt       = 2 # Time offset (records data every dt seconds)
-n        = 0 # index
-data1    = []
+n        = 0 # ind
 
-fname       = "../data/file.txt"
+fname       = "/home/pi/Precipitable-Water-Model/data/file.txt"
 f           = open(fname, "a+", buffering=1)
 
-ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
-
-#ser = serial.Serial(port="/dev/serial/by-id/usb-Arduino_LLC_Arduino_Micro-if00", baudrate=9600)
-
-def rw_Serial(data):
+def rw_Serial(data, ser):
     line = ser.readline();
     line = line.decode("utf-8")
-    try:
-        data.append(float(line.strip("\n")))
-    except ValueError:
-        pass
 
-while n < 10:
-    now      = datetime.now()
-    measure1 = datetime.strptime("17:37:30", "%H:%M:%S") + timedelta(seconds=(tau * n))
-    end      = measure1 + timedelta(seconds=dt)
-    send     = measure1 + timedelta(seconds=dt*2)
+    print(line)
+    reading = float(line.strip("\n"))
+    data.append(reading)
 
-    if now.time().strftime("%H:%M:%S") == str(measure1.time()):
-        rw_Serial(data1)
-    elif now.time().strftime("%H:%M:%S") == str(end.time()):
-        f.write(str(measure1.time()) + "," + str(round(mean(array(data1)), 2)) + "\n")
-        n = n + 1
-        data1 = []
-        continue
-    else:
-        continue
+measure1 = datetime.now() + timedelta(seconds=10) 
+#measure1 = datetime.strptime('14:13:00', '%H:%M:%S')
+def time_trigger():
+	while True:
+	    ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
+	    data1    = []
+	    now1      = datetime.now()
+	    #measure1 = datetime.strptime("10:16:30", "%H:%M:%S") + timedelta(seconds=(tau * n))
+	    if now1.time().strftime("%H:%M:%S") == str(measure1.time().strftime("%H:%M:%S")):
+	      ser.write(bytes('Y', 'utf-8'))
+	      rw_Serial(data1, ser)
+	      f.write(str(measure1.time().strftime('%H:%M:%S')) + "," + str(round(mean(array(data1)), 2)) + "\n")
+	      measure1 = (measure1 + timedelta(seconds=(tau)))
+	      subprocess.call('scp /home/pi/Precipitable-Water-Model/data/file.txt ursa_major@129.138.36.123:/home/projects/Precipitable-Water-Model/auto', shell=True)
+	      continue
+	    else:
+	        continue
+
+def user_trigger():
+	while True:
+		io = input("Activate?\n>> ")
+		if io == "y":
+			ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
+			data1 = []
+			ser.write(bytes('Y', 'utf-8'))
+			rw_Serial(data1, ser)
+			print(round(mean(array(data1)), 2))
+		else:
+			print("URSA Major not active")
+			break
+
+user_trigger()
